8e7a7dff1a0b6e9b9f5dccbf8b6dadd96ff77fc9
Daniel Stockman <daniels@zillow.com>
2014-06-12 10:44:51 -0700

    Move test.run() into before() phase.

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,4 +1,4 @@
-/*global describe, it */
+/*global describe, before, it */
 'use strict';
 var path = require('path');
 var yeoman = require('yeoman-generator');
@@ -7,22 +7,24 @@ describe('yui generator', function () {
     var TMP_DIR = path.join(__dirname, 'temp');
     var APP_DIR = path.join(__dirname, '../app');
 
-    it('creates expected files', function (done) {
-        var expected = [
+    before(function (done) {
+        yeoman.test
+            .run(APP_DIR)
+            .inDir(TMP_DIR)
+            .onEnd(done);
+    });
+
+    it('creates expected files', function () {
+        yeoman.assert.file([
             'BUILD.md',
             'README.md',
             'Gruntfile.js',
+            'bower.json',
+            'package.json',
             '.editorconfig',
             '.gitignore',
             '.jshintrc',
             '.yeti.json'
-        ];
-
-        yeoman.test.run(APP_DIR)
-            .inDir(TMP_DIR)
-            .onEnd(function () {
-                yeoman.assert.file(expected);
-                done();
-            });
+        ]);
     });
 });
